214d58688859c33bce27f0baf44aad47cdb852b0
Eyepatch420 <142022740+Eyepatch420@users.noreply.github.com>
2026-05-23 14:40:57 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       if(s.size()!=t.size()){
+        return false;
+       }
+       unordered_map<char,int> mp;
+
+       for(char i: s){
+        mp[i]++;
+       }
+
+       for(int i = 0; i<t.size(); i++){
+        if(mp.count(t[i]==0)){
+            return false;
+        }
+        mp[t[i]]--;
+        if(mp[t[i]]<0){
+            return false;
+        }
+       }
+       return true;
+    }
+};
