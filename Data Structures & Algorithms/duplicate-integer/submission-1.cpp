253a079c5f21f26a12043aec066b5ec53ea987ce
Eyepatch420 <142022740+Eyepatch420@users.noreply.github.com>
2026-06-01 02:00:47 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        set<int> ans;
+        int n=nums.size();
+        for(int i: nums){
+            ans.insert(i);
+        }
+        int m= ans.size();
+        if(m==n){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+};
